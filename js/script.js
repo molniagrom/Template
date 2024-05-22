b2bb70a7ce8e10d1be6f1c1c6f15bd70abde10cb
molnia_grom <mgrom250@gmail.com>
2024-05-22 10:25:09 +0300

    added without__JS and UI components.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+// HTML + CSS
+
 document.addEventListener("DOMContentLoaded", () => {
     fetch('header.html')
         .then(response => response.text())
@@ -11,3 +14,21 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById('footer').innerHTML = data;
         });
 });
+
+// HTML + CSS + JS
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var btn = document.getElementById("btn");
+    var more = document.getElementById("more");
+
+    if(dots.style.display === "none") {
+        dots.style.display="inline";
+        more.style.display="none";
+        btn.innerHTML="Подробнее";
+    } else {
+        dots.style.display="none";
+        more.style.display="inline";
+        btn.innerHTML="Скрыть";
+    }
+}
